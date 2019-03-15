dfc16064d046874493b95ae7708b26f73fb4f24e
Bocity <haolovej@vip.qq.com>
2019-03-15 11:11:26 +0800

    update load data

---
+++ A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/hash_policy.hpp>
+#include <ext/pb_ds/priority_queue.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define REP(i, x, n) for (int i = (x); i < (n); ++i)
+#define PER(i, x, n) for (int i = (n) -1; i >= x; --i)
+#define endl "\n"
+#define DE puts("----------")
+#define CASE(i, ans) cout << "Case #" << (i) << ": " << (ans) << "\n"
+#define lowbit(x) ((x) & (-(x)))
+#define sqr(x) ((x) * (x))
+#define eps 1e-9
+#define PI acos(-1.0)
+#define MOD 1000000007
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+__gnu_pbds::cc_hash_table<string, int> hash1;
+__gnu_pbds::gp_hash_table<string, int> hash2;
+__gnu_pbds::priority_queue<int> pq;
+__gnu_pbds::tree<int, null_type, less<int>, rb_tree_tag> rbtree;
+typedef bitset<1005> Bitset;
+const int maxn = 10005;
+ll n, m, k;
+const long long mod = 1000000007;
+int a[10000];
+void put(ll n){
+    cout << n << endl;
+}
+void input(){
+    cin >> n;
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    input();
+    put(n);
+    return 0;
+}
